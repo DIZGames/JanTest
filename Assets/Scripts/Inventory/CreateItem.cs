1aee452ca6805f9e3610f9e7d76da928b4c0f42f
DIZGames <dizgamecompany@gmail.com>
2016-05-06 22:33:41 +0200

    InventarTest_Minimap
    asdasd

---
+++ Assets/Scripts/Inventory/CreateItem.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class CreateItem{
+
+
+
+    [MenuItem("Assets/Create/Item")]
+    public static void CreateMyAsset()
+    {
+        Items asset = ScriptableObject.CreateInstance<Items>();
+
+        AssetDatabase.CreateAsset(asset, "Assets/ItemDatabase/Item.asset");
+        AssetDatabase.SaveAssets();
+
+        EditorUtility.FocusProjectWindow();
+
+        Selection.activeObject = asset;
+    }
+
+
+}
---
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour {
+
+    public List<Items> inventory = new List<Items>();
+
+
+	// Use this for initialization
+	void Start () {
+	    //inventory.Add()
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
